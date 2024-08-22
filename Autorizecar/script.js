b781a15c2113ee614b5b9a84c7e3669b642cbe11
Larissa Guimaraes <111644936+Lellis17@users.noreply.github.com>
2024-08-21 21:11:28 -0300

    Add files via upload

---
+++ Autorizecar/script.js
@@ -0,0 +1,7 @@
+<script>
+
+    document.getElementById("form-login").addEventListener("submit", function (event) {
+      event.preventDefault();
+      window.location.href = this.action; // Redireciona para a URL definida no atributo 'action' do formulário
+  
+  </script>
